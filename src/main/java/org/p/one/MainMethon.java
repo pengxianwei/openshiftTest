f72ef54b44390e1035aed61431a0c8519b42dc32
pengxianwei <p631788162@gmail.com>
2017-12-28 15:29:36 +0800

    更新代码

--- src/main/java/org/p/one/MainMethon.java
+++ src/main/java/org/p/one/MainMethon.java
@@ -2,10 +2,11 @@ package org.p.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 public class MainMethon extends SpringBootServletInitializer{
 @Override
 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
