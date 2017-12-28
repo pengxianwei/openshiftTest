872e92529bf03242734a0480dd056e706af3e40b
pengxianwei <p631788162@gmail.com>
2017-12-28 15:07:06 +0800

    测试一下

---
+++ src/main/java/org/p/one/MainMethon.java
@@ -0,0 +1,18 @@
+package org.p.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MainMethon extends SpringBootServletInitializer{
+@Override
+protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	// TODO Auto-generated method stub
+	return builder.sources(MainMethon.class);
+}
+public static void main(String[] args) {
+	SpringApplication.run(MainMethon.class, args);
+}
+}
